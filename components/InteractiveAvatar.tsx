935b10279bf30081d1dbb5656df376ee07e3fc17
Joby <jobyrao@heygen.com>
2024-09-06 20:59:55 -0700

    Feat/livekit (#9)
    * feat: using version 2.0 skd
    
    * feat: using version 2.0 skd
    
    * feat: using version 2.0 skd

--- components/InteractiveAvatar.tsx
+++ components/InteractiveAvatar.tsx
@@ -1,9 +1,5 @@
-import { AVATARS, VOICES } from "@/app/lib/constants";
-import {
-  Configuration,
-  NewSessionData,
-  StreamingAvatarApi,
-} from "@heygen/streaming-avatar";
+import type { StartAvatarResponse } from "@heygen/streaming-avatar";
+import StreamingAvatar, {AvatarQuality, StreamingEvents} from "@heygen/streaming-avatar";
 import {
   Button,
   Card,
@@ -14,63 +10,24 @@ import {
   Select,
   SelectItem,
   Spinner,
-  Tooltip,
+  Chip,
 } from "@nextui-org/react";
-import { Microphone, MicrophoneStage } from "@phosphor-icons/react";
-import { useChat } from "ai/react";
-import clsx from "clsx";
-import OpenAI from "openai";
 import { useEffect, useRef, useState } from "react";
+import { usePrevious } from 'ahooks'
 import InteractiveAvatarTextInput from "./InteractiveAvatarTextInput";
-
-const openai = new OpenAI({
-  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
-  dangerouslyAllowBrowser: true,
-});
+import { AVATARS } from "@/app/lib/constants";
 
 export default function InteractiveAvatar() {
   const [isLoadingSession, setIsLoadingSession] = useState(false);
   const [isLoadingRepeat, setIsLoadingRepeat] = useState(false);
-  const [isLoadingChat, setIsLoadingChat] = useState(false);
   const [stream, setStream] = useState<MediaStream>();
   const [debug, setDebug] = useState<string>();
+  const [knowledgeId, setKnowledgeId] = useState<string>("");
   const [avatarId, setAvatarId] = useState<string>("");
-  const [voiceId, setVoiceId] = useState<string>("");
-  const [data, setData] = useState<NewSessionData>();
+  const [data, setData] = useState<StartAvatarResponse>();
   const [text, setText] = useState<string>("");
-  const [initialized, setInitialized] = useState(false); // Track initialization
-  const [recording, setRecording] = useState(false); // Track recording state
   const mediaStream = useRef<HTMLVideoElement>(null);
-  const avatar = useRef<StreamingAvatarApi | null>(null);
-  const mediaRecorder = useRef<MediaRecorder | null>(null);
-  const audioChunks = useRef<Blob[]>([]);
-  const { input, setInput, handleSubmit } = useChat({
-    onFinish: async (message) => {
-      console.log("ChatGPT Response:", message);
-
-      if (!initialized || !avatar.current) {
-        setDebug("Avatar API not initialized");
-        return;
-      }
-
-      //send the ChatGPT response to the Interactive Avatar
-      await avatar.current
-        .speak({
-          taskRequest: { text: message.content, sessionId: data?.sessionId },
-        })
-        .catch((e) => {
-          setDebug(e.message);
-        });
-      setIsLoadingChat(false);
-    },
-    initialMessages: [
-      {
-        id: "1",
-        role: "system",
-        content: "You are a helpful assistant.",
-      },
-    ],
-  });
+  const avatar = useRef<StreamingAvatar | null>(null);
 
   async function fetchAccessToken() {
     try {
@@ -79,113 +36,96 @@ export default function InteractiveAvatar() {
       });
       const token = await response.text();
       console.log("Access Token:", token); // Log the token to verify
+
       return token;
     } catch (error) {
       console.error("Error fetching access token:", error);
-      return "";
     }
+
+    return "";
   }
 
   async function startSession() {
     setIsLoadingSession(true);
-    await updateToken();
-    if (!avatar.current) {
-      setDebug("Avatar API is not initialized");
-      return;
-    }
+    const newToken = await fetchAccessToken();
+    avatar.current = new StreamingAvatar({
+      token: newToken,
+    });
+    avatar.current.on(StreamingEvents.AVATAR_START_TALKING, (e) => {
+      console.log("Avatar started talking", e);
+    });
+    avatar.current.on(StreamingEvents.AVATAR_STOP_TALKING, (e) => {
+      console.log("Avatar stopped talking", e);
+    });
+    avatar.current.on(StreamingEvents.STREAM_DISCONNECTED, () => {
+      console.log("Stream disconnected");
+      endSession();
+    });
     try {
-      const res = await avatar.current.createStartAvatar(
-        {
-          newSessionRequest: {
-            quality: "low",
-            avatarName: avatarId,
-            voice: { voiceId: voiceId },
-          },
-        },
-        setDebug
-      );
+      const res = await avatar.current.createStartAvatar({
+        quality: AvatarQuality.Low,
+        avatarName: avatarId,
+        knowledgeId: knowledgeId,
+      });
+
       setData(res);
-      setStream(avatar.current.mediaStream);
+      avatar.current?.on(StreamingEvents.STREAM_READY, (event) => {
+        console.log('Stream ready:', event.detail);
+        setStream(event.detail);
+      });
     } catch (error) {
       console.error("Error starting avatar session:", error);
-      setDebug(
-        `There was an error starting the session. ${voiceId ? "This custom voice ID may not be supported." : ""}`
-      );
+    } finally {
+      setIsLoadingSession(false);
     }
-    setIsLoadingSession(false);
   }
-
-  async function updateToken() {
-    const newToken = await fetchAccessToken();
-    console.log("Updating Access Token:", newToken); // Log token for debugging
-    avatar.current = new StreamingAvatarApi(
-      new Configuration({ accessToken: newToken })
-    );
-
-    const startTalkCallback = (e: any) => {
-      console.log("Avatar started talking", e);
-    };
-
-    const stopTalkCallback = (e: any) => {
-      console.log("Avatar stopped talking", e);
-    };
-
-    console.log("Adding event handlers:", avatar.current);
-    avatar.current.addEventHandler("avatar_start_talking", startTalkCallback);
-    avatar.current.addEventHandler("avatar_stop_talking", stopTalkCallback);
-
-    setInitialized(true);
-  }
-
-  async function handleInterrupt() {
-    if (!initialized || !avatar.current) {
-      setDebug("Avatar API not initialized");
-      return;
-    }
-    await avatar.current
-      .interrupt({ interruptRequest: { sessionId: data?.sessionId } })
-      .catch((e) => {
-        setDebug(e.message);
-      });
-  }
-
-  async function endSession() {
-    if (!initialized || !avatar.current) {
-      setDebug("Avatar API not initialized");
-      return;
-    }
-    await avatar.current.stopAvatar(
-      { stopSessionRequest: { sessionId: data?.sessionId } },
-      setDebug
-    );
-    setStream(undefined);
-  }
-
   async function handleSpeak() {
     setIsLoadingRepeat(true);
-    if (!initialized || !avatar.current) {
+    if (!avatar.current) {
       setDebug("Avatar API not initialized");
+
       return;
     }
     await avatar.current
-      .speak({ taskRequest: { text: text, sessionId: data?.sessionId } })
+      .speak({ text: text, sessionId: data?.session_id! })
       .catch((e) => {
         setDebug(e.message);
       });
     setIsLoadingRepeat(false);
   }
+  async function handleInterrupt() {
+    if (!avatar.current) {
+      setDebug("Avatar API not initialized");
+
+      return;
+    }
+    await avatar.current
+      .interrupt({ sessionId: data?.session_id! })
+      .catch((e) => {
+        setDebug(e.message);
+      });
+  }
+  async function endSession() {
+    if (!avatar.current) {
+      setDebug("Avatar API not initialized");
+
+      return;
+    }
+    await avatar.current.stopAvatar({
+      sessionId: data?.session_id!,
+    });
+    setStream(undefined);
+  }
+  const previousText = usePrevious(text);
+  useEffect(() => {
+    if (!previousText && text) {
+      avatar.current?.startListening({ sessionId: data?.session_id! });
+    } else if (previousText && !text) {
+      avatar?.current?.stopListening({ sessionId: data?.session_id! });
+    }
+  }, [text, previousText]);
 
   useEffect(() => {
-    async function init() {
-      const newToken = await fetchAccessToken();
-      console.log("Initializing with Access Token:", newToken); // Log token for debugging
-      avatar.current = new StreamingAvatarApi(
-        new Configuration({ accessToken: newToken, jitterBuffer: 200 })
-      );
-      setInitialized(true); // Set initialized to true
-    }
-    init();
-
     return () => {
       endSession();
     };
@@ -200,54 +140,6 @@ export default function InteractiveAvatar() {
       };
     }
   }, [mediaStream, stream]);
-
-  function startRecording() {
-    navigator.mediaDevices
-      .getUserMedia({ audio: true })
-      .then((stream) => {
-        mediaRecorder.current = new MediaRecorder(stream);
-        mediaRecorder.current.ondataavailable = (event) => {
-          audioChunks.current.push(event.data);
-        };
-        mediaRecorder.current.onstop = () => {
-          const audioBlob = new Blob(audioChunks.current, {
-            type: "audio/wav",
-          });
-          audioChunks.current = [];
-          transcribeAudio(audioBlob);
-        };
-        mediaRecorder.current.start();
-        setRecording(true);
-      })
-      .catch((error) => {
-        console.error("Error accessing microphone:", error);
-      });
-  }
-
-  function stopRecording() {
-    if (mediaRecorder.current) {
-      mediaRecorder.current.stop();
-      setRecording(false);
-    }
-  }
-
-  async function transcribeAudio(audioBlob: Blob) {
-    try {
-      // Convert Blob to File
-      const audioFile = new File([audioBlob], "recording.wav", {
-        type: "audio/wav",
-      });
-      const response = await openai.audio.transcriptions.create({
-        model: "whisper-1",
-        file: audioFile,
-      });
-      const transcription = response.text;
-      console.log("Transcription: ", transcription);
-      setInput(transcription);
-    } catch (error) {
-      console.error("Error transcribing audio:", error);
-    }
-  }
 
   return (
     <div className="w-full flex flex-col gap-4">
@@ -290,6 +182,14 @@ export default function InteractiveAvatar() {
             <div className="h-full justify-center items-center flex flex-col gap-8 w-[500px] self-center">
               <div className="flex flex-col gap-2 w-full">
                 <p className="text-sm font-medium leading-none">
+                  Custom Knowledge ID (optional)
+                </p>
+                <Input
+                  value={knowledgeId}
+                  onChange={(e) => setKnowledgeId(e.target.value)}
+                  placeholder="Enter a custom knowledge ID"
+                />
+                <p className="text-sm font-medium leading-none">
                   Custom Avatar ID (optional)
                 </p>
                 <Input
@@ -314,29 +214,6 @@ export default function InteractiveAvatar() {
                   ))}
                 </Select>
               </div>
-              <div className="flex flex-col gap-2 w-full">
-                <p className="text-sm font-medium leading-none">
-                  Custom Voice ID (optional)
-                </p>
-                <Input
-                  value={voiceId}
-                  onChange={(e) => setVoiceId(e.target.value)}
-                  placeholder="Enter a custom voice ID"
-                />
-                <Select
-                  placeholder="Or select one from these example voices"
-                  size="md"
-                  onChange={(e) => {
-                    setVoiceId(e.target.value);
-                  }}
-                >
-                  {VOICES.map((voice) => (
-                    <SelectItem key={voice.voice_id} textValue={voice.voice_id}>
-                      {voice.name} | {voice.language} | {voice.gender}
-                    </SelectItem>
-                  ))}
-                </Select>
-              </div>
               <Button
                 size="md"
                 onClick={startSession}
@@ -351,60 +228,19 @@ export default function InteractiveAvatar() {
           )}
         </CardBody>
         <Divider />
-        <CardFooter className="flex flex-col gap-3">
+        <CardFooter className="flex flex-col gap-3 relative">
           <InteractiveAvatarTextInput
-            label="Repeat"
-            placeholder="Type something for the avatar to repeat"
+            label="Chat"
+            placeholder="Type something for the avatar to respond"
             input={text}
             onSubmit={handleSpeak}
             setInput={setText}
             disabled={!stream}
             loading={isLoadingRepeat}
           />
-          <InteractiveAvatarTextInput
-            label="Chat"
-            placeholder="Chat with the avatar (uses ChatGPT)"
-            input={input}
-            onSubmit={() => {
-              setIsLoadingChat(true);
-              if (!input) {
-                setDebug("Please enter text to send to ChatGPT");
-                return;
-              }
-              handleSubmit();
-            }}
-            setInput={setInput}
-            loading={isLoadingChat}
-            endContent={
-              <Tooltip
-                content={!recording ? "Start recording" : "Stop recording"}
-              >
-                <Button
-                  onClick={!recording ? startRecording : stopRecording}
-                  isDisabled={!stream}
-                  isIconOnly
-                  className={clsx(
-                    "mr-4 text-white",
-                    !recording
-                      ? "bg-gradient-to-tr from-indigo-500 to-indigo-300"
-                      : ""
-                  )}
-                  size="sm"
-                  variant="shadow"
-                >
-                  {!recording ? (
-                    <Microphone size={20} />
-                  ) : (
-                    <>
-                      <div className="absolute h-full w-full bg-gradient-to-tr from-indigo-500 to-indigo-300 animate-pulse -z-10"></div>
-                      <MicrophoneStage size={20} />
-                    </>
-                  )}
-                </Button>
-              </Tooltip>
-            }
-            disabled={!stream}
-          />
+          {
+            text && <Chip className='absolute right-16 top-6'>Listening</Chip>
+          }
         </CardFooter>
       </Card>
       <p className="font-mono text-right">
