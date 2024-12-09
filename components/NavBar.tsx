4c917542d80dea427449f03d16e3eaf75d7f84f2
white-gandalf <83410223+white-gandalf@users.noreply.github.com>
2024-12-09 15:46:10 -0500

    Change the header of the web UI, remove all logos and links
    Remove all logos and links from the header of the web UI.
    
    * Remove `HeyGenLogo` and `Link` imports.
    * Remove `NavbarBrand` and `NavbarContent` components.
    * Keep only the `ThemeSwitch` component in the header.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,70 +1,14 @@
 "use client";
 
 import {
-  Link,
   Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
 } from "@nextui-org/react";
-import { GithubIcon, HeyGenLogo } from "./Icons";
 import { ThemeSwitch } from "./ThemeSwitch";
 
 export default function NavBar() {
   return (
     <Navbar className="w-full">
-      <NavbarBrand>
-        <Link isExternal aria-label="HeyGen" href="https://app.heygen.com/">
-          <HeyGenLogo />
-        </Link>
-        <div className="bg-gradient-to-br from-sky-300 to-indigo-500 bg-clip-text ml-4">
-          <p className="text-xl font-semibold text-transparent">
-            HeyGen Interactive Avatar SDK NextJS Demo
-          </p>
-        </div>
-      </NavbarBrand>
-      <NavbarContent justify="center">
-        <NavbarItem className="flex flex-row items-center gap-4">
-          <Link
-            isExternal
-            color="foreground"
-            href="https://labs.heygen.com/interactive-avatar"
-          >
-            Avatars
-          </Link>
-          <Link
-            isExternal
-            color="foreground"
-            href="https://docs.heygen.com/reference/list-voices-v2"
-          >
-            Voices
-          </Link>
-          <Link
-            isExternal
-            color="foreground"
-            href="https://docs.heygen.com/reference/new-session-copy"
-          >
-            API Docs
-          </Link>
-          <Link
-            isExternal
-            color="foreground"
-            href="https://help.heygen.com/en/articles/9182113-interactive-avatar-101-your-ultimate-guide"
-          >
-            Guide
-          </Link>
-          <Link
-            isExternal
-            aria-label="Github"
-            href="https://github.com/HeyGen-Official/StreamingAvatarSDK"
-            className="flex flex-row justify-center gap-1 text-foreground"
-          >
-            <GithubIcon className="text-default-500" />
-            SDK
-          </Link>
-          <ThemeSwitch />
-        </NavbarItem>
-      </NavbarContent>
+      <ThemeSwitch />
     </Navbar>
   );
 }
